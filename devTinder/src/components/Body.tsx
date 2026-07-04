f5dece9bc9169a8229cd1c540dab630c1c72da6b
SohamDandekar <dandekarsoham274@gmail.com>
2026-07-04 18:52:19 +0530

    Redirect user to login page incase they are not authenticated

--- devTinder/src/components/Body.tsx
+++ devTinder/src/components/Body.tsx
@@ -1,8 +1,37 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
 import NavBar from "./NavBar"
 import Footer from "./Footer"
+import { BASE_URL } from "../utils/constants"
+import axios from "axios"
+import { useDispatch, useSelector } from "react-redux"
+import { addUser } from "../utils/userSlice"
+import { useEffect } from "react"
 
 const Body = () => {
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store:any) => store.user);
+
+  const fetchUser = async () => {
+    try{
+        const res = await axios.get(BASE_URL + "/profile/view", 
+          {withCredentials: true});
+        dispatch(addUser(res.data));
+    }catch(err: unknown){
+        if(axios.isAxiosError(err) && err?.status === 401){
+          navigate("/login");
+        }
+        console.error(err);
+    }
+  }
+
+  useEffect(() => {
+    if(!userData){
+        fetchUser();
+    }
+  }, []);
+
   return (
     <div>
         <NavBar />
--- devTinder/src/components/Feed.tsx
+++ devTinder/src/components/Feed.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 
 const Feed = () => {
+
+  const userData = useSelector((store:any) => store.user);
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    if(!userData)
+        navigate("/login");
+  },[]);
+
   return (
     <div>Feed</div>
   )
